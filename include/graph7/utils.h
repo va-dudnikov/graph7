7f82998becdfd7a237fa878a82b5f9a506f13cbb
Vladislav Dudnikov <dudnikov.vladislav@gmail.com>
2020-03-13 17:26:43 +0400

    Documentation

--- include/graph7/utils.h
+++ include/graph7/utils.h
@@ -1,3 +1,7 @@
+/*!
+ * \file
+ * \brief Misc useful utils.
+ */
 #ifndef GRAPH7_UTILS_H
 #define GRAPH7_UTILS_H
 
@@ -7,14 +11,41 @@ extern "C" {
 
 #include <graph7/def.h>
 
+/*!
+ * \brief Checking what processor byte order.
+ * \return
+ */
 uint8_t graph7_utils_endianness(void);
 
+/*!
+ * \brief Reverse a buffer.
+ * \param[in] src Buffer.
+ * \param[in] length Buffer length.
+ */
 void graph7_utils_reverse(uint8_t *src, size_t length);
 
+/*!
+ * \brief Integer division and apply ceiling.
+ * \param[in] numerator
+ * \param[in] denominator
+ * \return Number.
+ */
 size_t graph7_utils_ceiling_div(size_t numerator, size_t denominator);
 
+/*!
+ * \brief Counting the number of bits in a number.
+ * \param[in] x Number.
+ * \return The number of bits.
+ */
 size_t graph7_utils_count_bits(size_t x);
 
+/*!
+ * \brief graph7_utils_bytes_start_with
+ * \param src
+ * \param start
+ * \param start_length
+ * \return
+ */
 bool graph7_utils_bytes_start_with(const uint8_t *src, const uint8_t *start, size_t start_length);
 
 #endif // GRAPH7_UTILS_H
